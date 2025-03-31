4e94b493531c571d6c40fd1b5cb1a64eea2ffae1
nick <8650180292@my.lerna.by>
2025-03-31 14:46:25 +0300

    update main.py add @app.exception_handler(MicroblogHTTPException)

--- app/main.py
+++ app/main.py
@@ -1,12 +1,27 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
+from app.core.exceptions import MicroblogHTTPException
 from app.core.logging import log
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    # # Подключение к БД
+    # await db.connect()
+    #
+    # # Создание демо-данных в dev-режиме
+    # if settings.DEBUG and not settings.TESTING:
+    #     async with db.session() as session:
+    #         await create_demo_data(session)
+    #
+    # yield
+    #
+    # # Завершение работы
+    # await db.disconnect()
+
     log.info("Starting application...")
     try:
         yield
@@ -26,3 +41,18 @@ async def log_requests(request: Request, call_next):
     response = await call_next(request)
     log.info(f"Response: {response.status_code}")
     return response
+
+
+@app.exception_handler(MicroblogHTTPException)
+async def microblog_exception_handler(
+        request: Request, exc: MicroblogHTTPException
+):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "result": False,
+            "error_type": exc.error_type,
+            "error_message": exc.detail,
+            **exc.extra
+        }
+    )
