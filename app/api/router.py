22d6e281faf727ac2a159ef4ebffb2352b3a45d1
nick <8650180292@my.lerna.by>
2025-04-02 12:52:08 +0300

    update routers & delete old routers

--- app/api/router.py
+++ app/api/router.py
@@ -1,9 +1,9 @@
-"""Главный роутер API, агрегирующий роутеры разных версий."""
+"""Главный агрегатор API роутеров."""
 
 from fastapi import APIRouter
 
-# Импортируем роутер версии v1
-from app.api.v1.router import router_v1
+from app.api.v1.router import router_v1  # Импортируем роутер v1
+from app.core.config import settings  # Импортируем настройки для префикса
 
 # Создаем главный роутер API
 api_router = APIRouter()
@@ -11,6 +11,7 @@ api_router = APIRouter()
 # Подключаем роутер v1 с префиксом /v1
 # Теперь все пути v1 будут начинаться с /api/v1/...
 api_router.include_router(router_v1, prefix="/v1")
+# api_router.include_router(router_v1, prefix=settings.API_V1_PREFIX)
 
 # Сюда можно будет добавить роутеры для других версий API в будущем
 # from app.api.v2.router import router_v2
